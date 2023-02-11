22ae1628864356d8c203b44a750bb183ffa09243
Juan Ballén <91291820+JCBallen@users.noreply.github.com>
2023-02-11 10:30:49 -0500

    class feb/9

---
+++ CV-Class3/transformacion.py
@@ -0,0 +1,64 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread(
+    'c:/Users/juanc/Documents/PyProyects/Computer-Vision/CV-Class3/lenna.png', cv.IMREAD_GRAYSCALE)
+
+# cv.imshow('lol',img)
+im2 = img.copy()
+wid = img.shape[1]
+hgt = img.shape[0]
+# print(wid,hgt)
+
+
+def negativo(img):
+    global wid, hgt, im2
+    for i in range(wid-1):
+        for j in range(hgt-1):
+            im2[i][j] = 255-img[i][j]
+    return im2
+
+
+def logaritmica(img, c):
+    global wid, hgt, im2
+    for i in range(wid):
+        for j in range(hgt):
+            im2[i][j] = c*np.log10(1+img[i][j])
+    return im2
+
+
+def potencia(img, c, gamma):
+    global wid, hgt, im2
+    for i in range(wid):
+        for j in range(hgt):
+            im2[i][j] = c*(img[i][j]**gamma)
+    return im2
+
+
+def intensidad(img, brillo):
+    # global wid, hgt, im2
+    for i in range(wid):
+        for j in range(hgt):
+            im2[i][j] = img[i][j]+brillo
+    return im2
+
+
+def contraste(img, i_min, i_max):
+    for i in range(wid):
+        for j in range(hgt):
+            im2[i][j] = (img[i][j]-i_min)*255/(i_max-i_min)
+    return im2
+
+
+# Requests
+# img2 = negativo(img)
+# img2 = logaritmica(img,54)
+# img2 = potencia(img, 5, 2)
+# img2 = intensidad(img, -45)
+img2 = contraste(img, 100, 250)
+
+
+img2 = np.array(img2)
+cv.imshow('lol', img2)
+
+cv.waitKey(0)
--- First Contact/read-imgs&vids.py
+++ First Contact/read-imgs&vids.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 
 # ! Reading Images
-img = cv.imread('lenna.png')  # reads from this folder
+img = cv.imread('c:/Users/juanc/Documents/PyProyects/Computer-Vision/First Contact/lenna.png')  # reads from this folder
 
 cv.imshow('img', img)  # show the media
 
 cv.waitKey(0)  # wait for a specific delay to press a key (0 is infinite)
 
 # ! Reading Videos
-capture = cv.VideoCapture('evermore.mp4')  # parameter int webcam *usually 0
+capture = cv.VideoCapture('c:/Users/juanc/Documents/PyProyects/Computer-Vision/First Contact/evermore.mp4')  # parameter int webcam *usually 0
 # To show a video we need a loop an show it frame by frame
 while True:
     isTrue, frame = capture.read()  # frame + confirmation if frame was read or not
